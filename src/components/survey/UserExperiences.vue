ae677fc4b2ca77e2dd5277fe5756b71de615ccc1
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-08-05 17:21:43 +0300

    Add POST/GET request to REST API

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="onLoad">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  mounted () {
+    this.onLoad();
+  },
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    onLoad() {
+      fetch('https://vue-test-http-a9f40-default-rtdb.firebaseio.com/surveys.json')
+      .then(res => res.json())
+      .then(data => {
+        const results = [];
+
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating
+          });
+        }
+
+        this.results = results;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
